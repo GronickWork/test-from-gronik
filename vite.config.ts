c142d73e7fc86c1f033a244788c4d22aa39e4f98
Gronik4 <gronweb@yandex.ru>
2025-03-05 18:32:42 +0300

    Fix package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./test-from-gronik",
+  base: "./test-from-gronik/",
   plugins: [react()],
 })
